34532eac151268d3871d38fd54efb755bd1d5809
thuktenyouzer-dev <thuktenyouzer@gmail.com>
2026-09-26 19:29:29 +0600

    Unit V Lab 5: REST API for restaurants, menus, orders, and stats

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Parse form data
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 // Routes
 const indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
@@ -25,4 +25,7 @@ app.use('/', indexRoutes);
 app.listen(PORT, () => {
   console.log(`Campus Eats running at http://localhost:${PORT}`);
 });
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
+
 
